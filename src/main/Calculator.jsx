ffb21df0317733a671a551621c7e6691b86e5531
Lucas Alves de Souza <mazekeel@gmail.com>
2022-11-29 12:29:03 -0300

    bugfixes with decimal numbers

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -48,7 +48,7 @@ export default class Calculator extends Component {
             const values = [...this.state.values]
             values[i] = newValue
             this.setState({ values })
-        }
+        } 
     }
  
     render(){
